3f7a8eb02f141721e428430ed5afde0300f37044
Sunny10922 <79443223+Sunny10922@users.noreply.github.com>
2021-03-09 14:48:16 +0530

    Commit

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -3,19 +3,18 @@ define([
 ], function (
     Postmonger
 ) {
-    'use strict'; 
+    'use strict';
 
     var connection = new Postmonger.Session();
     var authTokens = {};
     var payload = {};
+
     $(window).ready(onRender);
 
     connection.on('initActivity', initialize);
     connection.on('requestedTokens', onGetTokens);
     connection.on('requestedEndpoints', onGetEndpoints);
 
-    connection.on('clickedNext', save);
-   
     function onRender() {
         // JB will respond the first time 'ready' is called with 'initActivity'
         connection.trigger('ready');
@@ -26,11 +25,12 @@ define([
     }
 
     function initialize(data) {
+        console.log('--Inside iitialize');
         console.log(data);
         if (data) {
             payload = data;
         }
-        
+
         var hasInArguments = Boolean(
             payload['arguments'] &&
             payload['arguments'].execute &&
@@ -44,8 +44,8 @@ define([
 
         $.each(inArguments, function (index, inArgument) {
             $.each(inArgument, function (key, val) {
-                
-              
+                console.log('--inArgument--');
+                console.log(inArgument);
             });
         });
 
@@ -54,31 +54,39 @@ define([
             text: 'done',
             visible: true
         });
+
+
     }
 
     function onGetTokens(tokens) {
+        // Response: tokens = { token: <legacy token>, fuel2token: <fuel api token> }
         console.log(tokens);
         authTokens = tokens;
     }
 
     function onGetEndpoints(endpoints) {
+        // Response: endpoints = { restHost: <url> } i.e. "rest.s1.qa1.exacttarget.com"
         console.log(endpoints);
     }
 
     function save() {
-        var postcardURLValue = $('#postcard-url').val();
-        var postcardTextValue = $('#postcard-text').val();
+        console.log('--Inside Save--');
+        // 'payload' is initialized on 'initActivity' above.
+        // Journey Builder sends an initial payload with defaults
+        // set by this activity's config.json file.  Any property
+        // may be overridden as desired.
+        payload.name = name;
 
         payload['arguments'].execute.inArguments = [{
             "tokens": authTokens,
-            "emailAddress": "{{Contact.Attribute.PostcardJourney.EmailAddress}}"
+            "emailAddress": "sunny.bansal@customactivity.com"
         }];
-        
+
         payload['metaData'].isConfigured = true;
 
         console.log(payload);
+
         connection.trigger('updateActivity', payload);
     }
-
 
 });
